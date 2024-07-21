3ff3bb6700491280fc34f05a659927bf50f24a52
IgorMontezuma20 <igorminranda17@gmail.com>
2024-07-21 20:04:30 -0300

    feat: Criação da navbar e componente avatar

--- src/app/shared/font-awesome-icons.ts
+++ src/app/shared/font-awesome-icons.ts
@@ -1,5 +1,7 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faAirbnb } from '@fortawesome/free-brands-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 export const fontAwesomeIcons: IconDefinition[] = [
-  faAirbnb
+  faAirbnb,
+  faUser
 ];
