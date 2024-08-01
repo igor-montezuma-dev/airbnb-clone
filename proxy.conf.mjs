6d9700ff448716fde017223554eb8548c48c1dd8
IgorMontezuma20 <igorminranda17@gmail.com>
2024-07-31 21:05:37 -0300

    feat: Implementação do login

--- proxy.conf.mjs
+++ proxy.conf.mjs
@@ -1,7 +1,7 @@
 export default [
   {
-    context: ['/api', 'oauth2', '/login', '/assets'],
-    target: 'http://localhost:8080',
-    secure : true,
-  }
-]
+    context: ["/api", "/oauth2", "/login", "/assets"],
+    target: "http://localhost:8080",
+    secure: true,
+  },
+];
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { DialogService } from 'primeng/dynamicdialog';
 import { MenuModule } from 'primeng/menu';
 import { ToolbarModule } from 'primeng/toolbar';
+import { AuthService } from '../../core/auth/auth.service';
+import { User } from '../../core/model/user.model';
 import { ToastService } from './../services/toast.service';
 import { AvatarComponent } from './avatar/avatar.component';
 import { CategoryComponent } from './category/category.component';
@@ -30,26 +32,73 @@ export class NavbarComponent implements OnInit {
   dates: string = 'Qualquer semana';
 
   private ToastService = inject(ToastService);
+  protected authService: AuthService = inject(AuthService);
+
+  login = () => this.authService.login();
+  logout = () => this.authService.logout();
 
   currentMenuItems: MenuItem[] | undefined = [];
+  protected connectedUser: User = { email: this.authService.notConnected };
 
-  ngOnInit(): void {
-    this.fetchMenu();
-    this.ToastService.send({ severity: 'info', summary: 'Bem vindo!'});
+  constructor() {
+    effect(() => {
+      if (this.authService.fetchUser().status === 'OK') {
+        this.connectedUser = this.authService.fetchUser().value!;
+        this.currentMenuItems = this.fetchMenu();
+      }
+    });
   }
 
-  private fetchMenu() {
-    return [
-      {
-        label: 'Cadastre-se',
-        styleClass: 'font-bold',
-      },
-      {
-        label: 'Entrar',
-      },
-    ];
+  ngOnInit(): void {
+    this.authService.fetch(false);
+    //this.currentMenuItems = this.fetchMenu();
+    //this.ToastService.send({ severity: 'info', summary: 'Bem vindo!' });
+  }
+
+  private fetchMenu(): MenuItem[] {
+    if (this.authService.isAuthenticated()) {
+
+      return [
+        {
+          label: 'Minhas propriedades',
+          routerLink: "landlord/properties",
+          visible: this.hasToBeLandlord(),
+        },
+        {
+          label: 'Minhas buscas',
+          routerLink: "booking",
+
+        },
+        {
+          label: 'Minhas reservas',
+          routerLink: "landlord/reservation",
+          visible: this.hasToBeLandlord(),
+        },
+        {
+          label: 'Sair',
+          command: () => this.logout(),
+        }
+      ]
+    }else{
+      return [
+        {
+          label: 'Cadastre-se',
+          styleClass: 'font-bold',
+          command: () => this.login(),
+        },
+        {
+          label: 'Entrar',
+          command: () => this.login()
+        },
+      ]
+    }
+
+  }
+
+  hasToBeLandlord(): boolean {
+    return this.authService.hasAnyAuthority("ROLE_LANDLORD");
   }
 
   // login() => this.authService.login();
-  // logout() => this.authService.login();
+
 }
